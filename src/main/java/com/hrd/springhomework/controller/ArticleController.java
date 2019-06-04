aa47c537dd16ea3d7c5fd425dc702006843cb817
Sokha <yornsokha168@gmail.com>
2019-06-04 13:06:17 +0700

    first commit

---
+++ src/main/java/com/hrd/springhomework/controller/ArticleController.java
@@ -0,0 +1,56 @@
+package com.hrd.springhomework.controller;
+
+import com.hrd.springhomework.repository.model.Article;
+import com.hrd.springhomework.service.ArticleService.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Controller
+public class ArticleController {
+
+    private ArticleService articleService;
+
+    @Autowired
+    public void setArticleService(ArticleService articleService){
+        this.articleService = articleService;
+        for (int i = 0; i < 5; i++)
+            articleService.add(new Article("Spring","Sokha", "Cambodia", "fdjks"));
+    }
+    @GetMapping({"/", "/article"})
+    public String index(Model model){
+        System.out.println(articleService.findAll().size());
+        model.addAttribute("articles", articleService.findAll());
+        return "/articles/index";
+    }
+
+    @GetMapping("/article/create")
+    public String create(Model model){
+        model.addAttribute("article", new Article());
+        return "/articles/create";
+    }
+
+    @PostMapping("/article/add")
+    public String add(@ModelAttribute Article article, @RequestParam MultipartFile file){
+
+        String filename= UUID.randomUUID().toString();
+        if(!file.isEmpty()){
+            try {
+                Files.copy(file.getInputStream(),Paths.get("C:\\server\\image",file.getOriginalFilename()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+//        return "redirect:/fileForm";
+        System.out.println(article.toString());
+        return "redirect:/article/create";
+    }
+}
